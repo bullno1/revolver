0e1368d67b4edef2e1d5e6a88496672261c3a378
bullno1 <thebullno1@gmail.com>
2014-11-30 18:22:14 +0800

    Allow recompilation of .dtl templates

--- src/rev_reloader.erl
+++ src/rev_reloader.erl
@@ -13,9 +13,16 @@ start_link() -> gen_server:start_link(?MODULE, [], []).
 % gen_server
 
 init([]) ->
-	CodeDirs = lists:usort([filename:dirname(Path) || {_Mod, Path} <- code:all_loaded(), Path =/= preloaded]),
+	RunningApplications = [Application || {Application, _, _} <- application:which_applications()],
+	AppResourceFiles = [code:where_is_file(lists:flatten(io_lib:format("~s.app", [App]))) || App <- RunningApplications],
+	AppProperties = [file:consult(ResourceFile) || ResourceFile <- AppResourceFiles],
+	ActiveModules = lists:usort(lists:flatten([proplists:get_value(modules, Properties) || {ok, [{application, _, Properties}]} <- AppProperties])),
+	_ = [code:ensure_loaded(Mod) || Mod <- ActiveModules],
+	ActiveModules = lists:usort(lists:flatten([proplists:get_value(modules, Properties) || {ok, [{application, _, Properties}]} <- AppProperties])),
+	ModuleMap = [{Mod, code:which(Mod)} || Mod <- ActiveModules],
+	CodeDirs = lists:usort([filename:dirname(Path) || {_Mod, Path} <- ModuleMap, Path =/= preloaded]),
 	SrcDirs = lists:usort([filename:dirname(proplists:get_value(source, Mod:module_info(compile))) ||
-	                       {Mod, Path} <- code:all_loaded(), Path =/= preloaded, erlang:function_exported(Mod, module_info, 0)]),
+	                       {Mod, Path} <- ModuleMap, Path =/= preloaded, erlang:function_exported(Mod, module_info, 0)]),
 	WatchableSrcDirs = [Dir || Dir <- SrcDirs, filelib:is_dir(Dir)],
 	WatchList = CodeDirs ++ WatchableSrcDirs,
 	error_logger:info_report([{watched_dirs, WatchList}]),
@@ -32,7 +39,12 @@ handle_cast(_Req, State) -> {stop, unexpected, State}.
 handle_info({line, Monitor, Path}, #state{monitor = Monitor} = State) ->
 	Extension = filename:extension(Path),
 	case Extension of
-		".erl" -> recompile(Path);
+		".erl" ->
+			Module = list_to_atom(filename:basename(Path, ".erl")),
+			recompile(Module, Path, fun compile_erl/3);
+		".dtl" ->
+			Module = list_to_atom(filename:basename(Path, ".dtl") ++ "_dtl"),
+			recompile(Module, Path, fun compile_dtl/3);
 		".beam" -> reload(Path);
 		_ -> ignore
 	end,
@@ -46,23 +58,32 @@ format_status(_Opt, [_PDict, State]) ->
 
 % private
 
-recompile(Path) ->
-	Module = list_to_atom(filename:basename(Path, ".erl")),
+recompile(Module, Path, CompileFun) ->
 	case erlang:function_exported(Module, module_info, 1) of
 		true ->
 			CompileOpts = proplists:get_value(options, Module:module_info(compile), []),
-			do_recompile(Module, Path, CompileOpts);
+			do_recompile(Module, Path, CompileFun, CompileOpts);
 		false ->
 			error_logger:warning_report([{ignored, Path}])
 	end.
 
-do_recompile(Module, Path, CompileOpts) ->
-	case compile:file(Path, [binary, return | CompileOpts]) of
+do_recompile(Module, Path, CompileFun, CompileOpts) ->
+	case CompileFun(Module, Path, [binary, return | CompileOpts]) of
 		{ok, _ModName, ModBin, Warnings} ->
 			file:write_file(code:which(Module), ModBin),
 			error_logger:info_report([{recompile, Path}, {warnings, Warnings}]);
 		{error, Errors, Warnings} ->
 			error_logger:error_report([{recompile, Path}, {errors, Errors}, {warnings, Warnings}])
+	end.
+
+compile_erl(_Module, Path, CompileOpts) -> compile:file(Path, CompileOpts).
+
+compile_dtl(Module, Path, CompileOpts) ->
+	case code:which(erlydtl) of
+		non_existing ->
+			{error, [no_erlydtl_compiler], []};
+		_ ->
+			erlydtl:compile_file(Path, Module, [{out_dir, false}, no_load, force_recompile | CompileOpts])
 	end.
 
 reload(Path) ->
